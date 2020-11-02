0e20b9669b931a15b27edb83de47d9743200cce0
Cole Michaels <cmichaelsd@gmail.com>
2020-11-01 19:45:53 -0800

    set up need for user to give access to external storage writing.

--- app/src/main/java/com/example/android/swapi/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/android/swapi/ui/splash/SplashFragment.kt
@@ -1,16 +1,22 @@
 package com.example.android.swapi.ui.splash
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import com.example.android.swapi.R
 
-class SplashFragment: Fragment() {
+const val PERMISSION_REQUEST_CODE = 1001
+
+class SplashFragment : Fragment() {
 
     private lateinit var navController: NavController
 
@@ -18,8 +24,44 @@ class SplashFragment: Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        displayMainFragment()
+
+        // If the application has permission to write to external storage
+        // Go to main fragment
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            displayMainFragment()
+        } else {
+            // Ask for user permission, this will trigger a callback
+            requestPermissions(
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+
+
         return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        // If the callback gives use the request code for out permission
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            // If the granted results are not empty and the first element in the granted array
+            // is permission granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Send the user to the main fragment
+                displayMainFragment()
+            } else {
+                Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun displayMainFragment() {
